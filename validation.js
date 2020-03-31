6417f8010c8db6472686de8a6157df049956d61e
sudhanshu uniyal <uniyal1001@gmail.com>
2020-03-31 09:44:16 +0530

    adding validation

--- validation.js
+++ validation.js
@@ -1,1 +1,24 @@
 // validation script here
+const inputs = document.querySelectorAll("input");
+
+const pattern={
+	telephone: /^\d{10}$/,
+	username: /^[a-z\d]{5,10}$/
+};
+
+function validation(field,regex){
+   if(regex.test(field.value)){
+   	field.className="valid";
+   }
+   else{
+   	field.className="invalid";
+   }
+}
+
+
+inputs.forEach((input)=>{
+	input.addEventListener("keyup",(e)=>{
+      // console.log(e.target.attributes.name.value);
+	   validation(e.target,pattern[e.target.attributes.name.value])
+	});
+});
